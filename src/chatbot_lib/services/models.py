b65b7f3c25eb27b538b2b442e49cb735d822a671
Gabriel Pinto Sasaki <A1087414@somoseducacao.com.br>
2024-02-08 12:06:38 -0300

    :feat: Created the implementation of the retrieve_context() method from the APIContextService class, and added the _headers attribute

--- src/chatbot_lib/services/models.py
+++ src/chatbot_lib/services/models.py
@@ -1,6 +1,7 @@
 import abc
 from typing import Any
 from langchain_core.language_models import BaseChatModel
+from langchain.chains import APIChain
 
 class ContextService(metaclass=abc.ABCMeta):
     def __init__(self,
@@ -15,12 +16,26 @@ class APIContextService(ContextService, metaclass=abc.ABCMeta):
     def __init__(self,
                  llm: BaseChatModel,
                  documentation: str,
-                 credentials: Any,
+                 credentials: dict[str, Any],
+                 headers: list[dict[str, Any]],
                  domains: list[str]) -> None:
         super().__init__(credentials)
         self._llm = llm
         self._documentation = documentation
+        self._headers = headers
         self._domains = domains
+
+    def retrieve_context(self, question: str, **kwargs) -> str:
+        chain = APIChain.from_llm_and_api_docs(
+            llm=self._llm,
+            api_docs=self._documentation,
+            headers=self._headers,
+            auth=self._credentials,
+            limit_to_domains=self._domains,
+            kwargs=kwargs
+        )
+
+        return chain.invoke(question)
 
 class RAGContextService(ContextService, metaclass=abc.ABCMeta):
     pass
